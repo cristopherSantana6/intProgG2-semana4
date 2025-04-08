468d83ff5b5901d7208a7b76c5c742f81eb6a242
cristopherSantana6 <cristopherrafael644@gmail.com>
2025-04-08 10:21:46 -0600

    actividades

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+#declarar variables
+nombre_completo="Jose Cristo Carvallo Herrera"
+edad=17
+altura=1.72
+soltero=True
+print(f"nombre completo: {nombre_completo}")
+print(f"tipo de dato: {type(nombre_completo)}")
+print(f"{edad} {type(edad)}")
+print(f"{altura:. 2f} {type(altura)}")
+print(f"{soltero} {type(soltero)}")
